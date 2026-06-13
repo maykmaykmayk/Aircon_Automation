d4654d2dafab92d9c12a428f45552c10ae6162ac
Joemike Grisola <167644452+maykmaykmayk@users.noreply.github.com>
2026-06-14 02:12:03 +0800

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-REM Build Aircon Calculator executable for Windows
-pyinstaller --onefile --console --name aircon-calculator main.py
-
-echo.
-echo Build complete. Check the dist folder for aircon-calculator.exe
-
